89f577a5962b4ebef1b7fb023f22e985d57abb11
Alouiscious Sims <info@reneesoul.com>
2019-06-07 07:41:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction
+
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-introduction_with_language
